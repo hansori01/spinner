71097ce6e342f2a1b2bd64a0e268bdfb04977e32
hansori01 <19sorihan88@gmail.com>
2021-02-09 20:55:43 +0000

    refactored spinner using for of loop

--- spinner2.js
+++ spinner2.js
@@ -1,40 +1,7 @@
-setTimeout(() => {
-  process.stdout.write('\r|   ');
-}, 100);
+let timeIncrements = 100;
+const animationChar = ['|', '/', '-', '\\', '|', '/', '-', '\\', '|', '/'];
 
-setTimeout(() => {
-  process.stdout.write('\r/   ');
-}, 300);
-
-setTimeout(() => {
-  process.stdout.write('\r-   ');
-}, 500);
-
-setTimeout(() => {
-  // Need to escape the backslash since it's a special character.
-  process.stdout.write('\r\\   ');
-}, 700);
-
-setTimeout(() => {
-  process.stdout.write('\r|   ');
-}, 900);
-
-setTimeout(() => {
-  process.stdout.write('\r/   ');
-}, 1100);
-
-setTimeout(() => {
-  process.stdout.write('\r-   ');
-}, 1300);
-
-setTimeout(() => {
-  process.stdout.write('\r\\   ');
-}, 1500);
-
-setTimeout(() => {
-  process.stdout.write('\r|   ');
-}, 1700);
-
-setTimeout(() => {
-  process.stdout.write('\r/   ');
-}, 1900);
+for (let char of animationChar) {
+  setTimeout(() => {process.stdout.write(`\r${char}   `);}, timeIncrements);
+  timeIncrements += 200;
+}
